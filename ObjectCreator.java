76c3247d2506e64198c7d9a8ca56f1add2d29d3d
Shayne Mujuru <shaynemujuru@gmail.com>
2019-03-22 02:59:53 -0600

    Initial Sender.java structure created and partly implemented

---
+++ ObjectCreator.java
@@ -0,0 +1,21 @@
+/**
+ * NAME: SHAYNE MUJURU
+ * UCID: 30029552
+ * TITLE: CPSC501 A3 : OBJECT CREATOR
+ * DESCRIPTION : 
+ * 
+ * This part of your system will create arbitrary objects under control of the user. 
+ * Allow the user to create one or more objects from a selection of objects using some sort of text-based menu system or GUI. 
+ * You must demonstrate that your system can handle the following kinds of objects:
+ * 
+ *      A simple object with only primitives for instance variables. 
+ * The user of your program must also beable to set the values for these fields.
+ *      An object that contains references to other objects. 
+ * Of course, these other objects must also be created at the same time, and their primitive instance variables must be settable by the user. 
+ * Your program must also be able to deal with circular references (i.e. objects connected in a graph).
+ *      An object that contains an array of primitives. 
+ * Allow the user to set the values for the array elements to arbitrary values.
+ *      An object that contains an array of object references. 
+ * The other objects must also be created at the same time.
+ *      An object that uses an instance of one of Java’s collection classes to refer to several other objects. These objects, too, must be created at the same time.
+ */
---
+++ Sender.java
@@ -0,0 +1,63 @@
+/**
+ * NAME: SHAYNE MUJURU
+ * UCID: 30029552
+ * TITLE: CPSC501 A3 : OBJECT CREATOR
+ * DESCRIPTION : Driver of Sender side of program
+ **/
+
+ import java.lang.reflect.*;
+ import org.jdom.*;
+ import java.util.Scanner;
+ import java.io.*;
+
+ public class Sender {
+
+    public static void main (String[] args) {
+
+        //Initialization
+        //ObjectCreator objCreator = new ObjectCreator();
+        Serializer serializer = new Serializer();
+        XMLOutputter xmlOut = new XMLOutputter();
+        Scanner in = new Scanner(System.in);
+
+        Object obj = null;
+        String objects = null;
+
+        //User selection options
+        
+        System.out.println("Pick one/multiple options, seperated by a space for type of objects to create:\n
+        1. Primitives\n
+        2. References\n
+        3. Array of Primitives\n
+        4. Array of Object References\n
+        5. Collections\n");
+
+        //Receive user input
+        objects = in.nextLine();
+
+        //Split input by using space as regex
+        String[] objectArray = objects.split(" ");
+
+        for (object : objectArray) {
+            switch (object) {
+                case "1" :
+                case "2" :
+                case "3" :
+                case "4" :
+                case "5" :
+                    {
+                        //Call createObject
+                        //objCreator.createObject(object);
+                        break;
+                    }
+                default :
+                    System.out.println("Choice out of accepted range");
+                    break;
+            }
+        }
+
+        //Serialize objects 
+
+        //Send objects
+    }
+ }
